d443174763c18e8af3be03e71d44f581f237bb59
John Garbero <john.garbero.itb@gmail.com>
2022-09-12 11:06:06 -0500

    myas

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,16 @@
 require_relative './stack'
 
 # your code here
+def balancing_parenth(str)
+
+    stack = Stack.new
+
+    str.chars.each do |c|
+        if stack.peek == '(' && == ')'
+            stack.pop
+        else
+            stack.push(c)
+        end
+    end
+    stack.size
+end
